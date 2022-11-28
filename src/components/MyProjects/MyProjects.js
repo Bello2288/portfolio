615f124708a4acc0603726bddfcd1ddd45af001e
Bello2288 <bellofattogregg@gmail.com>
2022-11-27 22:23:53 -0500

    added images directory, and added some wallpaper images. created links to final project and pixel perfect

--- src/components/MyProjects/MyProjects.js
+++ src/components/MyProjects/MyProjects.js
@@ -9,8 +9,13 @@ function MyProjects() {
     const navigate = useNavigate();
 
     return (  
-      <>
+      <main>
         <p>This is the My Projects Page</p>
+        <section className='project-list'>
+          <a href="https://github.com/Bello2288">Visit my GitHub Page</a>
+          <a href="https://on-deck-bello2288.herokuapp.com/">On Deck</a>
+          <a href="https://bello2288.github.io/Pixle-Perfect/">Pixel Perfect</a>
+        </section>
         <Button 
             className='back-btn' 
             type='button'
@@ -19,7 +24,7 @@ function MyProjects() {
         >
             Back
         </Button>         
-      </>
+      </main>
     );
   }
   
