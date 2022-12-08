5cea99c87e06272e2d9d30d22c4ec91cf7406850
Bello2288 <bellofattogregg@gmail.com>
2022-12-08 18:26:40 -0500

    added carousel and test images, added text to about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,14 +1,109 @@
 import './AboutMe.css';
 import { Button } from 'react-bootstrap';
 import { useNavigate} from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
+import city from '../../images/city.jpeg';
+import foggy from '../../images/foggy.jpeg';
+import moon from '../../images/moon.jpeg';
 
 function App() {
 
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
 
   return (
     <div className="App">
-      <p>This will be the about me page</p>
+      <section>
+        <h1>About Me</h1>
+        <p>Hey there, my name is Gregg!</p>
+        <p>I am a recent graduate of Carolina Code School. I live in South Carolina with my wife, three kids</p>
+        <p>and our 11 animals (I call it our petting zoo). We moved here seven years ago from New Hampshire.</p>
+        <p>When I'm not coding, I enjoy speading time with my family, being outside, playing board games,</p>
+        <p>watching movies, and being active in sports.</p>
+      </section>
+
+
+      <section className='flex-carousel'>
+        <Carousel fade className='carousel-container'>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={city}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 carousel-img"
+              src={foggy}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={moon}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+
+        {/* <Carousel fade className='carousel-container'>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={city}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={foggy}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={moon}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel> */}
+      </section>
+
       <Button
         className='back-btn'
         type='button'
