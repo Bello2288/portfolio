d1e91076b413f61573be734dd2c6bc08034842ae
Bello2288 <bellofattogregg@gmail.com>
2022-12-09 13:08:42 -0500

    added images for projects, modify links for projects, modify links to open in a new window

--- src/components/MyProjects/MyProjects.js
+++ src/components/MyProjects/MyProjects.js
@@ -1,6 +1,11 @@
 import './MyProjects.css';
 import Button from 'react-bootstrap/Button';
 import {useNavigate} from "react-router-dom";
+import github from '../../images/github.png'
+import on_deck from '../../images/on_deck.png'
+import news_app from '../../images/news_app.png'
+import pixel_perfect from '../../images/pixel_perfect.png'
+import calculator from '../../images/calculator.png'
 
 
 
@@ -12,10 +17,26 @@ function MyProjects() {
       <main>
         <p>This is the My Projects Page</p>
         <section className='project-list'>
-          <a href="https://github.com/Bello2288">Visit my GitHub Page</a>
-          <a href="https://on-deck-bello2288.herokuapp.com/">On Deck</a>
-          <a href="https://bello2288.github.io/django_newspaper_app/">Django News Paper App</a>
-          <a href="https://bello2288.github.io/Pixle-Perfect/">Pixel Perfect</a>
+          <div className='link-flex'>
+            <a href="https://github.com/Bello2288" target="_blank"><img className='link-img' src={github} alt="github_logo"></img></a>
+            <p>Visit my GitHub Page</p>
+          </div>
+          <div>
+            <a href="https://on-deck-bello2288.herokuapp.com/" target="_blank"><img className='link-img' src={on_deck} alt="on_deck"></img></a>
+            <p>On Deck</p>
+          </div>
+          <div>
+            <a href="https://newspaper-app-bello2288.herokuapp.com/" target="_blank"><img className='link-img' src={news_app} alt="news_app"></img></a>
+            <p>Django News Paper App</p>
+          </div>
+          <div>
+            <a href="https://bello2288.github.io/Pixle-Perfect/" target="_blank"><img className='link-img' src={pixel_perfect} alt="pixel_perfect"></img></a>
+            <p>Pixel Perfect</p>
+          </div>
+          <div>
+            <a href="https://bello2288.github.io/js-calculator/" target="_blank"><img className='link-img' src={calculator} alt="calculator"></img></a>
+            <p>JS Calculator</p>
+          </div>
         </section>
         <Button 
             className='back-btn' 
