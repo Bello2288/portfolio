95f680a6b9d3c91960896e0c66768b2391d565f0
Bello2288 <bellofattogregg@gmail.com>
2022-12-15 22:50:38 -0500

    added multiple font types to test

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -8,8 +8,8 @@ import ReactCurvedText from 'react-curved-text'
 function HomePage() { 
   return (
     <main className="footer-container">
-      <ReactCurvedText
-      className="name-tag"
+      {/* <ReactCurvedText
+      className="name-tags"
       width={370}
       height={300}
       cx={190}
@@ -18,9 +18,24 @@ function HomePage() {
       ry={100}
       startOffset={100}
       reversed={true}
+      textProps={{ style: { fontSize: '45' }}}
       text="Gregg Bellofatto"
-    />
+    /> */}
+
+
+
       <div className="name-tag">Gregg Bellofatto</div>
+      <div className="homemade-apple">Gregg Bellofatto</div>
+      <div className="kaushan-script">Gregg Bellofatto</div>
+      <div className="lobster">Gregg Bellofatto</div>
+      <div className="nothing-you-can-do">Gregg Bellofatto</div>
+      <div className="playball">Gregg Bellofatto</div>
+      <div className="rubik-80s-fade">Gregg Bellofatto</div>
+      <div className="black-ops-one">Gregg Bellofatto</div>
+      <div className="bree-serif">Gregg Bellofatto</div>
+      <div className="cabin-sketch">Gregg Bellofatto</div>
+      <div className="caveat">Gregg Bellofatto</div>
+      <div className="">Gregg Bellofatto</div>
       <Nav.Link className='aboutme-link' href="/about_me">About Me</Nav.Link>
       <Nav.Link className='projects-link' href="/my_projects">My Projects</Nav.Link>
       <Nav.Link className='contact-link' href='/contact_info'>Contact Info</Nav.Link>
