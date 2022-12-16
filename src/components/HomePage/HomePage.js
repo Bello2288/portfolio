0d1f3c5713f1f8e8fd56252f93fae728aa59a7e6
Bello2288 <bellofattogregg@gmail.com>
2022-12-15 22:01:14 -0500

    working on home page, setting up a curve in the text

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import './HomePage.css';
 import Nav from "react-bootstrap/Nav";
+import ReactCurvedText from 'react-curved-text'
 
 
 
@@ -7,7 +8,19 @@ import Nav from "react-bootstrap/Nav";
 function HomePage() { 
   return (
     <main className="footer-container">
-      <div className="name-tag">Gregg's Portfolio - Home Page</div>
+      <ReactCurvedText
+      className="name-tag"
+      width={370}
+      height={300}
+      cx={190}
+      cy={120}
+      rx={100}
+      ry={100}
+      startOffset={100}
+      reversed={true}
+      text="Gregg Bellofatto"
+    />
+      <div className="name-tag">Gregg Bellofatto</div>
       <Nav.Link className='aboutme-link' href="/about_me">About Me</Nav.Link>
       <Nav.Link className='projects-link' href="/my_projects">My Projects</Nav.Link>
       <Nav.Link className='contact-link' href='/contact_info'>Contact Info</Nav.Link>
